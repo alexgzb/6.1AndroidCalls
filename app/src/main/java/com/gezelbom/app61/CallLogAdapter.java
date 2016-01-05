99e70bf05f42679d261743229b7b59b4bf27bd63
Alex Gezelbom <alex.gezelbom@tre.se>
2016-01-06 00:39:29 +0100

    New and Fixed version of 6.1AndroidCalls

--- app/src/main/java/com/gezelbom/app61/CallLogAdapter.java
+++ app/src/main/java/com/gezelbom/app61/CallLogAdapter.java
@@ -4,23 +4,30 @@ import android.content.Context;
 import android.database.Cursor;
 import android.provider.CallLog;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 /**
- * Created by Alex
+ * Created by Alex Gezelbom
+ * <p>
+ * Custom Adapter to extend SimpleCursorAdapter to manage the values from the cursor
  */
 public class CallLogAdapter extends SimpleCursorAdapter {
 
     /**
-     * Experimenting with an enum type to replace a Switch case
+     * Experimenting with an enum type to instead of a Switch case
      */
     enum CallType {
 
         INCOMING(CallLog.Calls.INCOMING_TYPE),
-        MISSED(CallLog.Calls.MISSED_TYPE),
-        OUTGOING(CallLog.Calls.OUTGOING_TYPE);
+        OUTGOING(CallLog.Calls.OUTGOING_TYPE),
+        MISSED(CallLog.Calls.MISSED_TYPE);
 
         int number;
 
@@ -30,29 +37,39 @@ public class CallLogAdapter extends SimpleCursorAdapter {
 
     }
 
-    private Context context;
-    private int layout;
     private String[] from;
     private int[] to;
 
+    /**
+     * Constructor that simply saves the to and from parameters as fields
+     */
     public CallLogAdapter(Context context, int layout, Cursor c, String[] from, int[] to, int flags) {
         super(context, layout, c, from, to, flags);
-        this.context = context;
-        this.layout = layout;
         this.to = to;
         this.from = from;
     }
 
+    /**
+     * Custom bindView to bind the cursor values to the correct view.
+     * Custom to manage the special cases of Date and Type since these arent simple Strings
+     */
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        int typeColumn = cursor.getColumnIndex(CallLog.Calls.TYPE);
+
+        final int TYPE = 1;
+        final int DATE = 2;
+        final int typeColumn = cursor.getColumnIndex(CallLog.Calls.TYPE);
 
         for (int i = 0; i < to.length; i++) {
             String text;
             final View v = view.findViewById(to[i]);
-            if (i == typeColumn) {
-                //Get the callType String, use the fact that the int is in order of the index of the enum CallType
-                text = CallType.values()[(Integer.parseInt(cursor.getString(typeColumn))-1)].name();
+
+            if (i == TYPE) {
+                // Get the callType String, use the fact that the Constanta in CallLog.Calls are in order of the index of the enum CallType. Using -1 to match them
+                text = CallType.values()[cursor.getInt(typeColumn) - 1].toString();
+            } else if (i == DATE) {
+                // SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                text = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(new Date(cursor.getLong(cursor.getColumnIndex(from[i]))));
             } else {
                 text = cursor.getString(cursor.getColumnIndex(from[i]));
             }
--- app/src/main/java/com/gezelbom/app61/MainActivity.java
+++ app/src/main/java/com/gezelbom/app61/MainActivity.java
@@ -3,16 +3,23 @@ package com.gezelbom.app61;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CallLog;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
-
+/**
+ * Simple app that displays the call log in a ListView
+ * Clicking on a list item and then on the button Call will start an Intent to call the number
+ */
 public class MainActivity extends ListActivity {
     private static final String TAG = "MainActivity";
 
+    private TextView callNumber = null;
     private Button callButton = null;
     private ListView list = null;
     private Cursor cursor = null;
@@ -23,7 +30,29 @@ public class MainActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        callButton = (Button) findViewById(R.id.buttonCall);
+        callNumber = (TextView) findViewById(R.id.textView_number_to_call);
 
+        getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position,
+                                    long id) {
+                // Move cursor to correct position
+                cursor.moveToPosition(position);
+                // Get the Number of the listItem and set the Number to the callNumber TextView
+                callNumber.setText(cursor.getString(cursor.getColumnIndex(CallLog.Calls.NUMBER)));
+
+            }
+        });
+
+        getCallLog();
+
+    }
+
+    /**
+     * Method that creates a cursor from the calllog and chooses the Number, Type and Date as columns
+     * Custom SimpleCursorAdapter is used for the list
+     */
+    private void getCallLog() {
         cursor = managedQuery(CallLog.Calls.CONTENT_URI, null, null, null, null);
 
         String[] fromColumns = {CallLog.Calls.NUMBER, CallLog.Calls.TYPE, CallLog.Calls.DATE};
@@ -33,19 +62,16 @@ public class MainActivity extends ListActivity {
         super.setListAdapter(adapter);
 
         callButton = (Button) findViewById(R.id.buttonCall);
-
     }
 
+    /**
+     * Use the Number in callNumber textView to start an ACTION_CALL intent
+     * @param view
+     */
     public void makeCall(View view) {
         Intent i = new Intent(Intent.ACTION_CALL);
-//        String numberString = "tel:" + numberField.getText().toString().trim();
-//        i.setData(Uri.parse(numberString));
+        i.setData(Uri.parse("tel:" + callNumber.getText().toString()));
         startActivity(i);
-    }
-
-    public void searchContacts(View view) {
-
-
     }
 
 }
